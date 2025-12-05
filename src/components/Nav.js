955bd350efa8303e6df8d3e44286de1d2bbae390
Abhay Kamble <54679619+abzsd@users.noreply.github.com>
2025-12-05 15:52:15 +0530

    Revert "Claude/setup lemon routing 01 c nu l agooim e qbfpn j jme4x"

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Nav = () => {
     return (
         <nav className="site-nav" aria-label="Primary">
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/menu">Menu</Link></li>
-                <li><Link to="/booking">Reservations</Link></li>
-                <li><Link to="/order">Order Online</Link></li>
-                <li><Link to="/login">Login</Link></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/reservations">Reservations</a></li>
+                <li><a href="/order">Order Online</a></li>
+                <li><a href="/login">Login</a></li>
             </ul>
         </nav>
     );
